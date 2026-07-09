e12bd6f3219eebffc9df90f782474ac8336ca0a1
llian <zkllian.dev@gmail.com>
2026-07-09 09:41:25 +0000

    chore: remove debug test endpoint

--- api/stats/test.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.json({ ok: true, hasDb: !!process.env.RAILWAY_DATABASE_URL });
-}
